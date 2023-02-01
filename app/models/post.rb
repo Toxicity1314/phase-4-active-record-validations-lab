d37969294a3c6139214cc81f330956da405fedd7
Sean Welsh <s.welsh1314@gmail.com>
2023-02-01 12:02:45 -0500

    commit tree 684661ffd4869ec5e56a0fd789bdeb2fb9634771

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :clickbaity
+
+    def clickbaity
+        unless title.present? && title.match?(/(Won't Believe|Secret|Top \d+|Guess)\s\w*/)
+            errors.add(:title, "not clickbaity enough")
+        end
+    end
 end
